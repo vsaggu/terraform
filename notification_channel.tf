31d4f8e8de93935518d400debc705b58201a6403
vsaggu <vsaggu@gmail.com>
2020-03-21 14:29:18 +0530

    added varilable for alert metric

--- notification_channel.tf
+++ notification_channel.tf
@@ -1,8 +1,8 @@
 resource "google_monitoring_notification_channel" "notification_channel" {
-  count = "${length(var.notification_id)}"
-  display_name = "${var.notification_id[count.index]}"
+  #count = "${length(var.notification_id)}"
+  display_name = "${var.email_id}"
   type         = "email"
   labels = {
-    email_address = "${var.notification_id[count.index]}"
+    email_address = "${var.email_id}"
   }
 }
