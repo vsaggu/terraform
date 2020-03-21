5700c69d66a958889d28455d53ba8f6e3bc4d26b
vsaggu <vsaggu@gmail.com>
2020-03-21 14:23:33 +0530

    added varilable for alert metric

--- metrics.tf
+++ metrics.tf
@@ -2,8 +2,8 @@ resource "google_monitoring_alert_policy" "alert_policy" {
   count = "${var.alert_enable ? 1: 0 }"
   display_name = "${var.alert_name}"
   combiner     = "OR"
-  #notification_channels = ["${var.notification_id}"]
   notification_channels = ["${google_monitoring_notification_channel.notification_channel.*.id}"]
+  enabled = "true"
 
   conditions {
     display_name = "${var.alert_name}"
